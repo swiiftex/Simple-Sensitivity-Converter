95d1837cdc0fb6fea6c7237b429827ce017cef72
swiift <41881510+swiiftex@users.noreply.github.com>
2023-10-29 20:42:24 +0000

    initial upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCM360 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDPI = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBoxMethod = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFOV = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtCM360
+            // 
+            this.txtCM360.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCM360.Location = new System.Drawing.Point(138, 23);
+            this.txtCM360.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtCM360.Name = "txtCM360";
+            this.txtCM360.Size = new System.Drawing.Size(102, 38);
+            this.txtCM360.TabIndex = 0;
+            this.txtCM360.Text = "50";
+            this.txtCM360.TextChanged += new System.EventHandler(this.txtCM360_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "cm/360:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDPI);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtCM360);
+            this.groupBox1.Location = new System.Drawing.Point(63, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(266, 113);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtDPI
+            // 
+            this.txtDPI.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDPI.Location = new System.Drawing.Point(138, 66);
+            this.txtDPI.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtDPI.Name = "txtDPI";
+            this.txtDPI.Size = new System.Drawing.Size(102, 38);
+            this.txtDPI.TabIndex = 3;
+            this.txtDPI.Text = "1600";
+            this.txtDPI.TextChanged += new System.EventHandler(this.txtCM360_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 69);
+            this.label5.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 31);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "DPI:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBoxMethod);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtFOV);
+            this.groupBox2.Location = new System.Drawing.Point(12, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(390, 122);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // comboBoxMethod
+            // 
+            this.comboBoxMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMethod.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxMethod.FormattingEnabled = true;
+            this.comboBoxMethod.Items.AddRange(new object[] {
+            "Source",
+            "Unreal"});
+            this.comboBoxMethod.Location = new System.Drawing.Point(140, 37);
+            this.comboBoxMethod.Name = "comboBoxMethod";
+            this.comboBoxMethod.Size = new System.Drawing.Size(237, 24);
+            this.comboBoxMethod.TabIndex = 3;
+            this.comboBoxMethod.SelectedIndexChanged += new System.EventHandler(this.txtCM360_TextChanged);
+            this.comboBoxMethod.TextUpdate += new System.EventHandler(this.txtCM360_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 78);
+            this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "FOV:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Game:";
+            // 
+            // txtFOV
+            // 
+            this.txtFOV.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFOV.Location = new System.Drawing.Point(192, 75);
+            this.txtFOV.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtFOV.Name = "txtFOV";
+            this.txtFOV.Size = new System.Drawing.Size(102, 38);
+            this.txtFOV.TabIndex = 0;
+            this.txtFOV.Text = "90";
+            this.txtFOV.TextChanged += new System.EventHandler(this.txtCM360_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.txtOutput);
+            this.groupBox3.Location = new System.Drawing.Point(12, 249);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(390, 78);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 26);
+            this.label4.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 31);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Output:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(136, 23);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(241, 38);
+            this.txtOutput.TabIndex = 0;
+            this.txtOutput.Text = "0.0000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(406, 331);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "swiift\'s Simple Sensitivity Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCM360;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBoxMethod;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFOV;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtDPI;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        public Dictionary<string, double> StringValuesMap = new Dictionary<string, double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtCM360.Text = "";
+            // Add key-value pairs to the dictionary
+            StringValuesMap["Source"] = 41563.64;
+            StringValuesMap["Unreal"] = 365.796580;
+            StringValuesMap["VALORANT"] = 13062.857;
+            StringValuesMap["THE FINALS"] = 11592.210438;
+            StringValuesMap["BF5/2042"] = 398.977267;
+            StringValuesMap["CoD 2019-2024"] = 138545.45455;
+            StringValuesMap["CoD BO2"] = 41563.64;
+            StringValuesMap["CoD BO3"] = 43362.86989;
+
+            comboBoxMethod.Items.Clear();
+            foreach(string x in StringValuesMap.Keys)
+            {
+                comboBoxMethod.Items.Add(x);
+            }
+        }
+
+        private void txtCM360_TextChanged(object sender, EventArgs e)
+        {
+            txtOutput.Text = CalculateSens();
+        }
+
+        public string CalculateSens()
+        {
+            try
+            {
+                if (double.TryParse(txtCM360.Text, out double realWorldCm) &&
+                    int.TryParse(txtDPI.Text, out int mouseDpi))
+                {
+                    // Source engine games use a default FOV (Field of View) of 90 degrees.
+                    double inGameSensitivity = (StringValuesMap[comboBoxMethod.Text] / mouseDpi) / realWorldCm;
+
+                    return inGameSensitivity.ToString();
+                }
+                else
+                {
+                    return "ERR";
+                }
+            }
+            catch(Exception ex)
+            {
+                return "ERR";
+            }
+
+        }
+    }
+}
